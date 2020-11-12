a7f27797d9658a5639dc984cb3e42065b142759c
Marcel Chacon <machacon@expediagroup.com>
2020-11-12 15:34:10 -0600

    Fortune teller program If statement

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        int studentAge = 15;
-        double studentGPA = 3.45;
-
-        boolean hasPerfectAttendance = true;
-        String studentFirstName = "Marcel";
-        String studentLastName = "Chacon";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
-
-//        System.out.println(studentAge);
-//        System.out.println(studentGPA);
-//        System.out.println(studentFirstInitial);
-//        System.out.println(studentLastInitial);
-//        System.out.println(hasPerfectAttendance);
-//        System.out.println(studentFirstName);
-//        System.out.println(studentLastName);
-        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
-        System.out.println("What do you want to update the GPA to?");
-
+        System.out.println("Pick a number between 1 and 10");
         Scanner input = new Scanner(System.in);
-        studentGPA = input.nextDouble();
 
-        System.out.println(studentFirstName + " " + studentLastName + " has now a GPA of " + studentGPA);
+        int inputtedNum = input.nextInt();
 
+        if (inputtedNum < 5) {
+            System.out.println("Enjoy the good luck a friend bring you");
+        } else {
+            System.out.println("Your shoe selection will make you very happy today");
+        }
     }
 }
